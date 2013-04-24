251bc55543b3aeeac4b4fb3c63990b3ffc0852be
Satoru SATOH <satoru.satoh@gmail.com>
2013-04-24 21:17:51 +0900

    add a dirty hack to test this module

--- aux/runtest.sh
+++ aux/runtest.sh
@@ -10,6 +10,17 @@ fi
 
 which pep8 2>&1 > /dev/null && check_with_pep8=1 || check_with_pep8=0
 
+# Dirty hack! It is very ugly but I don't know how to fix this at once:
+setup () {
+    moddir=$topdir/anyconfig
+    base="https://raw.github.com/ssato/python-anyconfig/master/anyconfig"
+
+    for f in compat.py globals.py mergeabledict.py utils.py; do (cd $moddir && test -f $f || curl -O $base/$f); done
+    for f in base.py; do (cd $moddir/backend && test -f $f || curl -O $base/backend/$f); done
+}
+
+setup
+
 if test $# -gt 0; then
     test $check_with_pep8 = 1 && (for x in $@; do pep8 ${x%%:*}; done) || :
     PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg ${coverage_opts} $@
