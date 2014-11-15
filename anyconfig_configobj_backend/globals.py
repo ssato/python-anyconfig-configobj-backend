86a1f19a5e6d4e6ecb5b4ebbca743e9dd81ffdbc
Satoru SATOH <satoru.satoh@gmail.com>
2014-11-15 11:40:20 +0900

    add

---
+++ anyconfig_configobj_backend/globals.py
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2014 Satoru SATOH <ssato @ redhat.com>
+# License: MIT
+#
+PACKAGE = "anyconfig-configobj-backend"
+AUTHOR = "Satoru SATOH"
+VERSION = "0.0.3"
+
+# vim:sw=4:ts=4:et:
