30cf8f223a778c6b3848214e8e061bc9c5b9e4ff
Satoru SATOH <satoru.satoh@gmail.com>
2018-01-08 13:44:36 +0900

    refactor: merge the contents of .globals into __init__.py and clean up it

--- anyconfig_configobj_backend/__init__.py
+++ anyconfig_configobj_backend/__init__.py
@@ -1,12 +1,10 @@
-"""anyconfig configobj backend plugin.
+"""anyconfig configobj backend module.
 """
 from __future__ import absolute_import
-
-from .globals import AUTHOR, VERSION
 from .configobj import Parser
 
-__author__ = AUTHOR
-__version__ = VERSION
+__author__ = "Satoru SATOH"
+__version__ = "0.0.3"
 __all__ = ["Parser", ]
 
 # vim:sw=4:ts=4:et:
--- anyconfig_configobj_backend/globals.py
+++
@@ -1,4 +0,0 @@
-"""Globals of anyconfig configobj backend.
-"""
-AUTHOR = "Satoru SATOH"
-VERSION = "0.0.3"
